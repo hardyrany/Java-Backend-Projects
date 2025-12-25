2e65bc515d557865b38962c97cd6c80e4ef96ca3
hardyrany <hardymtf@gmail.com>
2025-12-25 16:30:20 -0100

    Lesson 5: Fix PUT endpoint, DTO conversion, and Optional handling

---
+++ APIs/DemoWebDB/demowebdb.v.5.12.25/src/test/java/com/example/demowebdb/DemowebdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demowebdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemowebdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
